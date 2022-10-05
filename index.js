9fc7bf1f4737566ffe844adaf006b38ca6b87bd9
Mahesh Reddy Kamala <maheshreddykamala@gmail.com>
2022-10-05 18:05:07 -0500

    address objet

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello Newman");
+const address = {
+    street : "cherlopalle",
+    city : "chittoor",
+    zipcode : "517129",
+}
+
+function showAddress(address){
+    for(let key in address)
+         console.log(key + ":" + address[key]);
+}
+
+showAddress(address);
