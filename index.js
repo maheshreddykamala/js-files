39df88d02ba69935969134616aef56f1128a67ec
Mahesh Reddy Kamala <maheshreddykamala@gmail.com>
2022-10-08 18:37:59 -0500

    implementing countOccurance function

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const numbers = [1,2,3,4,5,6,7];
+const numbers = [1,2,3,4,5,1,1,1,4,7];
 
-const output = move(numbers,3,20);
+const output = countOccurances(numbers,1);
 
 console.log(output);
 
-function move(numbers,index,offset){
+function countOccurances(numbers,searchElement){
   
-    const final = [...numbers];
-    if(index+offset < 0 || index+offset >= numbers.length){
-        console.error("invalid offset");
-        return;
-    }
-   final.splice(index,1);
-   final.splice(index+offset,0,numbers[index]);
-   return final;
+  return numbers.reduce((ocuurance,currentValue) => {
+          return currentValue === searchElement ? ++ocuurance : ocuurance;
+  },0);
+   
 }
