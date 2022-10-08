fa21d9f45b0f7d9fa44676ba40465c824cd77c42
Mahesh Reddy Kamala <maheshreddykamala@gmail.com>
2022-10-08 17:47:33 -0500

    implementing except function

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const numbers = [1,2,3,4];
-console.log(includes(numbers,30));
+const numbers = [1,2,3,4,1,1,2];
 
-function includes(numbers,searchElement){
-    return numbers.some( (number) => {
-        return number === searchElement;
+const output = except(numbers,[3]);
+
+console.log(output);
+
+function except(numbers,excluded){
+    const final = numbers.filter( (number) => {
+        for(let value of excluded){
+            if(number === value){
+                return false;
+            }
+        }
+        return true;
     })
+
+    return final;
 }
